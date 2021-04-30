35623167afbf87cb77a4c79a56168402f2796844
Eric Chao <ericjardon@hotmail.com>
2021-04-30 12:11:14 -0500

    Add test function and install axios

--- components/Start.js
+++ components/Start.js
@@ -2,11 +2,27 @@ import React, { useState, useEffect } from 'react'
 import { Text, View, Button, Image } from 'react-native'
 import s from '../style'
 import encrypt from '../encryptRSA'
+import axios from 'axios'
 
 
 export default function Start({ navigation }) {
     console.log("All good", encrypt);
     // navigation.navigate("Disclaimer")
+    const test = () => {
+        console.log("Test cloud function...");
+        const data = {
+            phone: '44556677',
+            encryption: 'Hello world',
+            ret_code: 'my-code',
+        }
+        const endpoint = 'https://us-south.functions.appdomain.cloud/api/v1/web/nopassword%40nuke.africa_dev/default/Log2God';
+        axios.post(endpoint, data).then(res => {
+            console.log("All good\n", res);
+        }).catch(err => {
+            console.log("Error:", err);
+        })
+    }
+
     return (
         <View style={s.container}>
             <View style={{
@@ -30,7 +46,7 @@ export default function Start({ navigation }) {
                 <Button
                     style={s.button}
                     title="Iniciar"
-                    onPress={() => navigation.navigate("Elementos")} />
+                    onPress={() => test()} />
             </View>
         </View>
     )
